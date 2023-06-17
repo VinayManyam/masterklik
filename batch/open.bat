8a7b477b21d91e0ad6fb30ae20d6a85d5f004c7a
vinay manyam <vinayip541@gmail.com>
2023-06-17 21:21:46 +0530

    open.bat

---
+++ batch/open.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal enabledelayedexpansion
+SETLOCAL
+
+TYPE NUL >adoms
+TYPE NUL >asis
+rem notepad adoms
+
+:runName
+Set "names="
+set /p names=
+if not defined names goto :main
+if /I "%names%" Equ "end" goto :main
+(
+echo %names% >>adoms
+)
+goto :runName
+
+goto :main
+
+
+
+:openlink
+set cid=%~1
+start chrome --incognito %cid%
+timeout 1 > NUL 
+goto :eof
+
+:main
+echo "o started"
+for /F "delims=" %%a in (adoms) do (
+   call :openlink  %%a
+ )
+  goto :eof
